135f2e938ffa81c0cbc756d33d818f06b07f57c6
Jason Melgoza <jasonmelgoza@gmail.com>
2012-04-28 16:51:51 -0700

    added partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+  
   protect_from_forgery
+  
+  def layout_by_resource
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Blue::Application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    get "sign_in", :to => "devise/sessions#new"
+  end
+
   root :to => "home#index"
   get "home/index"
 
